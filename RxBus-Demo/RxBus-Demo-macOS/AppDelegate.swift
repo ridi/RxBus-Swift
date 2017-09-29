837a2ec3761e4b2a336cf79ff1b845e17e9bf29b
DaVin Ahn <davin.ahn@ridi.com>
2017-09-29 12:00:11 +0900

    Update demo project

---
+++ RxBus-Demo/RxBus-Demo-macOS/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  RxBus-Demo-macOS
+//
+//  Created by Da Vin Ahn on 2017. 9. 29..
+//  Copyright © 2017년 Davin Ahn. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+}
---
+++ RxBus-Demo/RxBus-Demo-tvOS/ViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ViewController.swift
+//  RxBus-Demo-tvOS
+//
+//  Created by Da Vin Ahn on 2017. 9. 29..
+//  Copyright © 2017년 Davin Ahn. All rights reserved.
+//
+
+import UIKit
+import RxBus
+import RxSwift
+
+// Defining Events struct
+struct Events {
+    struct LoggedIn: BusEvent {
+        let userId: String
+    }
+    struct LoggedOut: BusEvent { }
+    struct Purchased: BusEvent {
+        let tid: Int
+    }
+}
+
+// Defining Custom Notification
+extension Notification.Name {
+    static let ViewControllerDidLoad = Notification.Name("ViewControllerDidLoadNotification")
+}
+
+class ViewController: UIViewController {
+    
+    private let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let bus = RxBus.shared
+        
+        // Event subscription/posting
+        
+        bus.asObservable(event: Events.LoggedIn.self).subscribe { event in
+            print("LoggedIn, userId = \(event.element!.userId)")
+        }.addDisposableTo(disposeBag)
+        bus.post(event: Events.LoggedIn(userId: "davin.ahn"))
+        
+        // Sticky events
+        
+        bus.post(event: Events.LoggedOut(), sticky: true)
+        bus.asObservable(event: Events.LoggedOut.self, sticky: true).subscribe { _ in
+            print("LoggedOut")
+        }.addDisposableTo(disposeBag)
+        
+        // Subscription priority
+        
+        bus.asObservable(event: Events.Purchased.self, sticky: false, priority: -1).subscribe { event in
+            print("Purchased(priority: -1), tid = \(event.element!.tid)")
+        }.addDisposableTo(disposeBag)
+        bus.asObservable(event: Events.Purchased.self, sticky: false, priority: 1).subscribe { event in
+            print("Purchased(priority: 1), tid = \(event.element!.tid)")
+        }.addDisposableTo(disposeBag)
+        bus.asObservable(event: Events.Purchased.self).subscribe { event in
+            print("Purchased(priority: 0 = default), tid = \(event.element!.tid)")
+        }.addDisposableTo(disposeBag)
+        bus.post(event: Events.Purchased(tid: 1001))
+        
+        // System Notification subscription
+        
+        bus.asObservable(notificationName: .UIScreenDidConnect).subscribe { event in
+            print("\(event.element!.name.rawValue), userInfo: \(event.element!.userInfo!)")
+        }.addDisposableTo(disposeBag)
+        
+        // Custom Notification subscription/posting
+        
+        bus.post(notificationName: .ViewControllerDidLoad, userInfo: ["message": "Hi~"], sticky: true)
+        bus.asObservable(notificationName: .ViewControllerDidLoad, sticky: true).subscribe { event in
+            print("\(event.element!.name.rawValue), userInfo: \(event.element!.userInfo!)")
+        }.addDisposableTo(disposeBag)
+        
+    }
+}
